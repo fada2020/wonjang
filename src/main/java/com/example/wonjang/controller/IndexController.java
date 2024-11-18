e62ee483521026209368380dacfdfc8e08b544e0
leehyeokju <hyeogjui4@gmail.com>
2024-11-18 16:08:06 +0900

    강의 리스트 페이지 추가

--- src/main/java/com/example/wonjang/controller/IndexController.java
+++ src/main/java/com/example/wonjang/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.example.wonjang.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/")
+import org.springframework.web.bind.annotation.GetMapping;
+@Slf4j
 @Controller
 public class IndexController {
+    @GetMapping("/")
     public String index(){
         return "index";
+    }
+    @GetMapping("/lecture/list")
+    public String lectureList(){
+        return "lecture/list";
     }
 }
