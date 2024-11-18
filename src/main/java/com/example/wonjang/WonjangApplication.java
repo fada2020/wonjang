a2bd9ba56f2a32b9be123bce9dd64e86bd7cac3b
leehyeokju <hyeogjui4@gmail.com>
2024-11-18 10:07:48 +0900

    일단 추가

--- src/main/java/com/example/wonjang/WonjangApplication.java
+++ src/main/java/com/example/wonjang/WonjangApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WonjangApplication {
-
+    // 시작
     public static void main(String[] args) {
         SpringApplication.run(WonjangApplication.class, args);
     }
