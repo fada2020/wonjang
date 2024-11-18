f3455234fea649c715b2c8cf7ac66a11c78cce23
leehyeokju <hyeogjui4@gmail.com>
2024-11-18 15:44:59 +0900

    security 추가

--- src/main/java/com/example/wonjang/WonjangApplication.java
+++ src/main/java/com/example/wonjang/WonjangApplication.java
@@ -2,8 +2,16 @@ package com.example.wonjang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableJpaRepositories
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화, 권한이 필요한 API에 @Secured 어노테이션 추가하여 권한 설정
 public class WonjangApplication {
     // 시작
     public static void main(String[] args) {
---
+++ src/main/java/com/example/wonjang/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.example.wonjang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/storage/**","/favicon.ico", "/css/**", "/js/**");
+    }
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors(Customizer.withDefaults())
+                //.csrf(AbstractHttpConfigurer::disable)
+                .csrf(t->t.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                //.headers((headerConfig) -> headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .headers((headerConfig) -> headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                //.requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers( "/**").permitAll()
+                                //.requestMatchers("/", "/member/*").hasRole(Role.USER.name())
+                                //.requestMatchers("/admins/**", "/api/v1/admins/**").hasRole(Role.ROLE_ADMIN.name())
+                                .anyRequest().authenticated()
+                )
+
+
+        ;
+        return http.build();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("POST", "PUT", "GET", "OPTIONS", "DELETE", "PATCH")); // or simply "*"
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
