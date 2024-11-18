6d329fa99e5923d862b8571c8e7dff1fd09d75ba
leehyeokju <hyeogjui4@gmail.com>
2024-11-18 16:12:53 +0900

    대충 기본 페이지 추가

--- src/main/java/com/example/wonjang/controller/IndexController.java
+++ src/main/java/com/example/wonjang/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.example.wonjang.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @Slf4j
 @Controller
 public class IndexController {
@@ -10,8 +12,23 @@ public class IndexController {
     public String index(){
         return "index";
     }
-    @GetMapping("/lecture/list")
-    public String lectureList(){
-        return "lecture/list";
+    @GetMapping("/lecture")
+    public String lectureIndex(){
+        return "lecture/index";
+    }
+    @GetMapping("/inquiry")
+    public String inquiryIndex(){
+        return "inquiry/index";
+    }
+    @GetMapping("/announce")
+    public String announceIndex(){
+        return "announce/index";
+    }
+    @GetMapping("/lecture/{id}")
+    public String lectureIndex(
+            @PathVariable int id
+    ){
+        System.out.println("id = " + id);
+        return "lecture/player";
     }
 }
