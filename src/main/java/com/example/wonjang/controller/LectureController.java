6111f5521265e116782797c70bc14865ba769f65
leehyeokju <hyeogjui4@gmail.com>
2024-11-19 14:03:22 +0900

    로그인 해야지 동영상 볼 수 있게 수정

--- src/main/java/com/example/wonjang/controller/LectureController.java
+++ src/main/java/com/example/wonjang/controller/LectureController.java
@@ -1,5 +1,7 @@
 package com.example.wonjang.controller;
 
+import com.example.wonjang.annotation.CurrentUser;
+import com.example.wonjang.dto.UserDto;
 import com.example.wonjang.model.Lecture;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -7,12 +9,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.wonjang.service.LectureService;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.Optional;
 
@@ -36,8 +40,13 @@ public class LectureController {
     @GetMapping("/{id}")
     public String lectureIndex(
             @PathVariable Long id
+            , @SessionAttribute(value = "admin", required = false) UserDto admin
+            , @SessionAttribute(value = "user", required = false) UserDto user
             , Model model
     ){
+        if (admin == null || user == null) {
+            return "redirect:/login";
+        }
         Optional<Lecture>optionalLecture = lectureService.findById(id);
         if(optionalLecture.isPresent()) {
             model.addAttribute("lecture", optionalLecture.get());
