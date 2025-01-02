3de1b376b3344a87114f76f7e608bdda070d2d14
leehyeokju <hyeogjui4@gmail.com>
2025-01-02 16:25:53 +0900

    수정

--- src/main/java/com/example/wonjang/controller/CustomErrorController.java
+++ src/main/java/com/example/wonjang/controller/CustomErrorController.java
@@ -2,18 +2,26 @@ package com.example.wonjang.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Slf4j
 @Controller
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         // 여기에 로직을 추가하여 500 오류 등 처리
-
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Throwable exception = (Throwable) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        String message = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        String requestUri = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        log.error("Status Code: {}", statusCode);
+        log.error("Exception: {}", exception);
+        log.error("Message: {}", message);
+        log.error("Request URI: {}", requestUri);
         return "redirect:/"; // customError.html 페이지로 이동
     }
 
